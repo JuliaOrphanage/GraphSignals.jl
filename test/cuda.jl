7bc78bcd37ac4edf24a6ff1d9e172d43863f08da
Yueh-Hua Tu <a504082002@gmail.com>
2020-11-17 11:33:27 +0800

    Add type in test

--- test/cuda.jl
+++ test/cuda.jl
@@ -1,28 +1,29 @@
-adj = [0 1 0 1;
+T = Float32
+adj = T[0 1 0 1;
        1 0 1 0;
        0 1 0 1;
        1 0 1 0]
-nf = rand(3, 4)
+nf = rand(T, 3, 4)
 
 @testset "cuda" begin
     fg = FeaturedGraph(cu(adj))
     @test has_graph(fg)
-    @test has_node_feature(fg) == false
-    @test has_edge_feature(fg) == false
-    @test has_global_feature(fg) == false
-    @test typeof(graph(fg)) == CuMatrix{Int64}
-    @test typeof(node_feature(fg)) == Fill{Int64,2,Tuple{Base.OneTo{Int64},Base.OneTo{Int64}}}
-    @test typeof(edge_feature(fg)) == Fill{Int64,2,Tuple{Base.OneTo{Int64},Base.OneTo{Int64}}}
-    @test typeof(global_feature(fg)) == Fill{Int64,1,Tuple{Base.OneTo{Int64}}}
+    @test !has_node_feature(fg)
+    @test !has_edge_feature(fg)
+    @test !has_global_feature(fg)
+    @test typeof(graph(fg)) == CuMatrix{T}
+    @test typeof(node_feature(fg)) == Fill{T,2,Tuple{Base.OneTo{Int64},Base.OneTo{Int64}}}
+    @test typeof(edge_feature(fg)) == Fill{T,2,Tuple{Base.OneTo{Int64},Base.OneTo{Int64}}}
+    @test typeof(global_feature(fg)) == Fill{T,1,Tuple{Base.OneTo{Int64}}}
 
 
     fg = FeaturedGraph(adj, cu(nf))
     @test has_graph(fg)
     @test has_node_feature(fg)
-    @test has_edge_feature(fg) == false
-    @test has_global_feature(fg) == false
-    @test typeof(graph(fg)) == CuMatrix{Float32}
-    @test typeof(node_feature(fg)) == CuMatrix{Float32}
-    @test typeof(edge_feature(fg)) == Fill{Float32,2,Tuple{Base.OneTo{Int64},Base.OneTo{Int64}}}
-    @test typeof(global_feature(fg)) == Fill{Float32,1,Tuple{Base.OneTo{Int64}}}
+    @test !has_edge_feature(fg)
+    @test !has_global_feature(fg)
+    @test typeof(graph(fg)) == CuMatrix{T}
+    @test typeof(node_feature(fg)) == CuMatrix{T}
+    @test typeof(edge_feature(fg)) == Fill{T,2,Tuple{Base.OneTo{Int64},Base.OneTo{Int64}}}
+    @test typeof(global_feature(fg)) == Fill{T,1,Tuple{Base.OneTo{Int64}}}
 end
