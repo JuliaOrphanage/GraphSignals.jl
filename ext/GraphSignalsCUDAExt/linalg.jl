194c2d9c857f293e2d0210a5d531d288c1d76d15
Yueh-Hua Tu <a504082002@gmail.com>
2023-09-06 00:10:10 +0800

    support only greater than v1.9

--- ext/GraphSignalsCUDAExt/linalg.jl
+++ ext/GraphSignalsCUDAExt/linalg.jl
@@ -1,17 +1,17 @@
-function adjacency_matrix(adj::CuSparseMatrixCSC{T}, ::Type{S}) where {T,S}
-    _dim_check(adj)
+function GraphSignals.adjacency_matrix(adj::CuSparseMatrixCSC{T}, ::Type{S}) where {T,S}
+    GraphSignals._dim_check(adj)
     return CuMatrix{S}(collect(adj))
 end
 
-function adjacency_matrix(adj::CuSparseMatrixCSC)
-    _dim_check(adj)
+function GraphSignals.adjacency_matrix(adj::CuSparseMatrixCSC)
+    GraphSignals._dim_check(adj)
     return CuMatrix(adj)
 end
 
 adjacency_matrix(adj::CuMatrix{T}, ::Type{T}) where {T} = adjacency_matrix(adj)
 
 function adjacency_matrix(adj::CuMatrix)
-    _dim_check(adj)
+    GraphSignals._dim_check(adj)
     return adj
 end
 
--- ext/GraphSignalsCUDAExt/sparsegraph.jl
+++ ext/GraphSignalsCUDAExt/sparsegraph.jl
@@ -1,7 +1,7 @@
-function SparseGraph(A::CuSparseMatrixCSC{Tv}, edges::AnyCuVector{Ti}, directed::Bool) where {Tv,Ti}
+function GraphSignals.SparseGraph(A::CuSparseMatrixCSC{Tv}, edges::AnyCuVector{Ti}, directed::Bool) where {Tv,Ti}
     E = maximum(edges)
     return SparseGraph{directed,typeof(A),typeof(edges),typeof(E)}(A, edges, E)
 end
 
-SparseGraph(A::CuSparseMatrixCSC, directed::Bool, ::Type{T}=eltype(A)) where {T} =
+GraphSignals.SparseGraph(A::CuSparseMatrixCSC, directed::Bool, ::Type{T}=eltype(A)) where {T} =
     SparseGraph(A, order_edges(A, directed=directed), directed, T)
